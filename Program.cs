65ae414a19868a6b355c51dfc5ea38b61bc3b881
rut559942 <rut559942@gmail.com>
2025-11-18 23:42:48 +0200

    Add DBproject to existing WEB-API-project repository

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalRowsAffected = 0;
+            string answer = "y";
+
+            while (answer.ToLower() == "y")
+            {
+                Console.Write("Enter product name: ");
+                string product_name = Console.ReadLine();
+
+                Console.Write("Enter product quantity: ");
+                int product_count = int.Parse(Console.ReadLine());
+
+                totalRowsAffected += Shop.InsertUser(product_name, product_count);
+
+                Console.Write("Continue entering another record? (y/n): ");
+                answer = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total {totalRowsAffected} Rows affected.");
+
+            List<Product> allProducts = Shop.GetAllProducts();
+
+            foreach (var p in allProducts)
+            {
+                Console.WriteLine($"Product: {p.ProductName}, Count: {p.ProductCount}");
+            }
+        }
+
+       
+    }
+}
---
+++ Shop.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DBproject
+{
+    class Shop
+    {
+        public static int InsertUser(string product_name, int product_count)
+        {
+            string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Shop;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
+
+            string query = "INSERT INTO Products (product_name, product_count) VALUES (@product_name, @product_count)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@product_name", product_name);
+                    command.Parameters.AddWithValue("@product_count", product_count);
+
+                    connection.Open();
+                    return command.ExecuteNonQuery();
+                }
+
+            }
+
+        }
+        public static List<Product> GetAllProducts()
+        {
+            string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Shop;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
+            string query = "SELECT product_name, product_count FROM Products";
+
+            List<Product> products = new List<Product>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Product p = new Product
+                        {
+                            ProductName = reader.GetString(0),
+                            ProductCount = reader.GetInt32(1)
+                        };
+                        products.Add(p);
+                    }
+                }
+            }
+            return products;
+
+
+        }
+
+    }
+}
